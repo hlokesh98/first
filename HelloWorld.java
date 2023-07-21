d3bb402ba4adf32d045d330fe1fb2d7387317fcf
hlokesh98 <139198252+hlokesh98@users.noreply.github.com>
2023-07-21 16:17:11 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+        System.out.println("pooja!");
+        System.out.println("Hegde!");
+        System.out.println("i love you!");
+        System.out.println("i love you!");
     }
 }
