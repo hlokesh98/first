0da4a59136cb8aa871a9a499c64b7041c72d83ba
EC2 Default User <ec2-user@ip-172-31-35-143.us-east-2.compute.internal>
2023-07-22 23:56:41 +0000

    2 line

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,14 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("pooja!");
+        System.out.println("pooja!");
+        System.out.println("pooja!");
+        System.out.println("Hegde!");
+        System.out.println("Today is 7/22/2023!");
+        
+        System.out.println("Hegde!");
+        System.out.println("Today is 7/22/2023!");
+        
         System.out.println("Hegde!");
         System.out.println("Today is 7/22/2023!");
         
